9de5c56bf47dc19674023ba056f84e9edaa208ba
sanket-gelel <sg692088@gmail.com>
2025-09-06 21:46:55 +0545

    Update q12.c

--- src/q12.c
+++ src/q12.c
@@ -1,1 +1,6 @@
-// Write a C program that prints all numbers from 1 to 100 using a for loop.
+#include <stdio.h>
+int main() {
+    for (int i = 1; i <= 100; i++)
+        printf("%d ", i);
+    return 0;
+}
